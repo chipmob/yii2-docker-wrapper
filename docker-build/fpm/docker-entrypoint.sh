14fa3f2268cb0367e01b90ac735100a0ecb010af
Eugene Mamonov <chipmob@gmail.com>
2020-10-24 20:32:23 +0300

    update

--- docker-build/fpm/docker-entrypoint.sh
+++ docker-build/fpm/docker-entrypoint.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env sh
 set -e
 
+mkdir -p /var/run/redis
+chmod 777 -R /var/run/redis/
+
 mkdir -p /var/log/cron
 touch /var/log/cron/php-fpm.log
-chmod 0777 /var/log/cron/php-fpm.log
+chmod 777 /var/log/cron/php-fpm.log
 
-chmod 0644 /etc/cron.d/php-fpm
+chmod 644 /etc/cron.d/php-fpm
 crontab /etc/cron.d/php-fpm
 cron -f &
 
